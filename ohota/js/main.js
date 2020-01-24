b43b9294cf81a2a91746d6b7270efcf03825f5be
Олександр Стаднік <sanyokstd@gmail.com>
2020-01-24 18:29:36 +0200

    1
    tabs and modal

--- ohota/js/main.js
+++ ohota/js/main.js
@@ -7,4 +7,58 @@ $(document).ready(function() {
   $(".burger-close").click(function() {
     $("#burger-menu").fadeOut();
   });
+
+  $(".modal").each(function() {
+    $(this).wrap('<div class="overlay"></div>');
+  });
+
+  $(".open-modal").on("click", function(e) {
+    e.preventDefault();
+    e.stopImmediatePropagation;
+
+    var $this = $(this),
+      modal = $($this).data("modal");
+
+    $(modal)
+      .parents(".overlay")
+      .addClass("open");
+    setTimeout(function() {
+      $(modal).addClass("open");
+    }, 350);
+
+    $(document).on("click", function(e) {
+      var target = $(e.target);
+
+      if ($(target).hasClass("overlay")) {
+        $(target)
+          .find(".modal")
+          .each(function() {
+            $(this).removeClass("open");
+          });
+        setTimeout(function() {
+          $(target).removeClass("open");
+        }, 350);
+      }
+    });
+  });
+
+  $(".close-modal").on("click", function(e) {
+    e.preventDefault();
+    e.stopImmediatePropagation;
+
+    var $this = $(this),
+      modal = $($this).data("modal");
+
+    $(modal).removeClass("open");
+    setTimeout(function() {
+      $(modal)
+        .parents(".overlay")
+        .removeClass("open");
+    }, 350);
+  });
+
+  $(".tab").click(function() {
+    $(".tab").removeClass("active");
+    $(this).addClass("active");
+  });
 });
